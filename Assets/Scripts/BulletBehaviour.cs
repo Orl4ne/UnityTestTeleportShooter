988e6597c68d7742e8aa853e5bdf3f77421bbe8b
Orlane Vermo <orlane_v@hotmail.com>
2020-05-29 13:06:51 +0200

    Initial commit

---
+++ Assets/Scripts/BulletBehaviour.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletBehaviour : MonoBehaviour
+{
+    public float initialForce;
+    public Transform playerTransform;
+    //public AudioSource impactSound;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<Rigidbody>().AddRelativeForce(Vector3.forward * initialForce, ForceMode.Impulse);
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if(collision.collider.CompareTag("Teleport"))
+        {
+            playerTransform.position = collision.GetContact(0).point;
+        }
+        //impactSound.Play();
+
+        if (!collision.collider.CompareTag("Bounce"))
+            Destroy(gameObject);
+    }
+}
---
+++ Assets/Scripts/GunBehaviour.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunBehaviour : MonoBehaviour
+{
+    public Transform bulletOrigin;
+    public Transform playerRoot;
+    public GameObject bulletPrefab;
+    public GameObject traceBulletPrefab;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            GameObject bullet = Instantiate(bulletPrefab, bulletOrigin.position, bulletOrigin.rotation);
+            bullet.GetComponent<BulletBehaviour>().playerTransform = playerRoot;
+        }
+        else if (Input.GetMouseButtonDown(1))
+        {
+            GameObject bullet = Instantiate(traceBulletPrefab, bulletOrigin.position, bulletOrigin.rotation);
+
+        }
+    }
+}
